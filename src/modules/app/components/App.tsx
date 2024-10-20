536405ee3b40f1f0af3c0933ce4bffee1186610a
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-10-20 22:11:04 +0300

    fix(app) refactor App.tsx

--- src/modules/app/components/App.tsx
+++ src/modules/app/components/App.tsx
@@ -1,5 +1,6 @@
 import {Todolist} from '../../todoList/components';
 
+import {AppFooter} from './footer';
 import {AppHeader} from './header';
 
 import s from './App.module.css';
@@ -9,6 +10,7 @@ export const App = () => {
     <div className={s.appContainer}>
       <AppHeader/>
       <Todolist/>
+      <AppFooter/>
     </div>
   );
 };
